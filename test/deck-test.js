7989cbea23808b97c1aee6b4f6c7e580b6a63225
Mike Frey <mike.frey@sportngin.com>
2014-10-20 20:35:59 -0500

    deal and cut tests for deck

--- test/deck-test.js
+++ test/deck-test.js
@@ -19,25 +19,58 @@ test('Deck Instance', function(t) {
     t.plan(1)
 
     var deck = new Deck()
-    var initialCards = deck.cards.reduce(concatNum, '')
+    var notExpected = deck.cards.reduce(concatNum, '')
     deck.shuffle()
-    var shuffledCards = deck.cards.reduce(concatNum, '')
-    t.notEqual(initialCards, shuffledCards, 'Shuffles cards')
+    var actual = deck.cards.reduce(concatNum, '')
+    t.notEqual(actual, notExpected, 'Shuffles cards')
   })
 
   t.test('deck.draw', function(t) {
     t.plan(2)
 
     var deck = new Deck()
-    var firstCard = deck.cards[0]
-    var card = deck.draw()
-    t.equal(firstCard, card, 'Draws a card off the top of the deck')
+    var expected = deck.cards[0]
+    var actual = deck.draw()
+    t.equal(actual, expected, 'Draws a card off the top of the deck')
 
-    var firstThree = deck.cards.slice(0, 3).reduce(concatNum, '')
-    var drawThree = deck.draw(3).reduce(concatNum, '')
-    t.deepEqual(firstThree, drawThree, 'Draws multiple cards from the top of the deck')
+    expected = deck.cards.slice(0, 3).reduce(concatNum, '')
+    actual = deck.draw(3).reduce(concatNum, '')
+    t.deepEqual(actual, expected, 'Draws multiple cards from the top of the deck')
   })
 
+  t.test('deck.deal', function(t) {
+    t.plan(4)
 
+    var deck = new Deck([1,2,3,4])
+    var expected = [[1,3],[2,4]]
+    var actual = deck.deal(2)
+    t.deepEqual(actual, expected, 'Deals cards into two stacks')
+
+    deck = new Deck([1,2,3,4,5,6])
+    expected = [[1,4],[2,5],[3,6]]
+    actual = deck.deal(3)
+    t.deepEqual(actual, expected, 'Deals cards into three stacks')
+
+    deck = new Deck([1,2,3,4,5])
+    expected = [[1,3],[2,4],[5]]
+    actual = deck.deal(2, true)
+    t.deepEqual(actual, expected, 'Can force even stacks with remainder stack')
+
+    deck = new Deck([1,2,3,4,5])
+    expected = [[1,3,5],[2,4]]
+    actual = deck.deal(2)
+    t.deepEqual(actual, expected, 'Will deal unevenly')
+
+  })
+
+  t.test('deck.cut', function(t) {
+    t.plan(1)
+
+    var deck = new Deck([1,2,3,4,5,6,7,8,9,0])
+    deck.cut()
+    var actual = deck.cards[deck.cards.indexOf(0)+1]
+    var expected = 1
+    t.equal(actual, expected, 'Cuts cards')
+  })
 
 })
